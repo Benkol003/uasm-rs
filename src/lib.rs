162f45e41972b2d34cf67210691053a889d13c14
unladencoconut@gmail.com <benjkollar03@gmail.com>
2026-06-30 10:58:13 +0100

    add note about const path

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![doc = include_str!("../readme.md")]
 
-/// The path to the built `uasm` executable.
+/// The path to the built `uasm` executable. for now this will be a &str until 
+/// const Path::new gets moved to stable - https://github.com/rust-lang/rust/issues/143773
 pub const UASM_PATH: &str = env!("UASM_PATH");
 
 #[cfg(test)]
